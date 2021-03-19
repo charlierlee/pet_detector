7f7b94829a9218c12528dfaa2e630d89720b4aa9
clee <clee@tuta.io>
2021-03-19 20:30:56 +0000

    add config

---
+++ setup.sh
@@ -0,0 +1,8 @@
+cd /home/pi/tensorflow1/models/research/object_detection
+export DISPLAY=:0
+export TWILIO_ACCOUNT_SID=a
+export TWILIO_AUTH_TOKEN=b
+export MY_DIGITS=+15555555555
+export MY_DIGITS2=+15555555555
+export TWILIO_DIGITS=+15555555555
+python3 Pet_detector.py &
