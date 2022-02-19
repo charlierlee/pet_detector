e2e9222a2e2a9d990024a55df540506a712a9050
Charlie Lee <clee@tuta.io>
2022-02-19 13:11:31 -0800

    add face detection

---
+++ face-svd-setup.sh
@@ -0,0 +1,5 @@
+python3 -m venv --system-site-packages ./venv
+source ./venv/bin/activate  # sh, bash, or zsh
+pip install --upgrade pip
+#pip list  # show packages installed within the virtual environment
+pip install -r requirements.txt
